42cd03ae21655d39aed6a7d9461e2d284041d5a4
Azarazzu <azzu9394@gmail.com>
2020-10-20 04:24:55 -0400

    second commit

--- src/com/company/PalindromeExample.java
+++ src/com/company/PalindromeExample.java
@@ -3,7 +3,7 @@ package com.company;
 class PalindromeExample{
     public static void main(String args[]){
         int r,sum=0,temp;
-        int n=321;//It is the number variable to be checked for palindrome
+        int n=422;//It is the number variable to be checked for palindrome
 
         temp=n;
         while(n>0){
